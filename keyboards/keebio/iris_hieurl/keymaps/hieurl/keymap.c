bf55722f4d02cd02f2c6e34e93e40d432e097019
hieurl <luongtronghieu@gmail.com>
2021-11-24 23:36:40 +0700

    - update keyboard layout

--- keyboards/keebio/iris_hieurl/keymaps/hieurl/keymap.c
+++ keyboards/keebio/iris_hieurl/keymaps/hieurl/keymap.c
@@ -17,9 +17,9 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
    //┌────────┬───────┬───────┬───────┬───────┬───────┐                      ┌───────┬───────┬───────┬───────┬───────┬───────┐
-      KC_ESC, KC_1,    KC_2,  KC_3,    KC_4,  KC_5 ,                          KC_6,  KC_7,    KC_8,  KC_9,   KC_0,    KC_PIPE,
+      KC_ESC, KC_1,    KC_2,  KC_3,    KC_4,  KC_5 ,                          KC_6,  KC_7,    KC_8,  KC_9,   KC_0,    KC_MINS,
    //├────────┼───────┼───────┼───────┼───────┼───────┤                      ├───────┼───────┼───────┼───────┼───────┼───────┤
-      TABHYPR_,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_MINS, 
+      KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_BSLS, 
    //├────────┼───────┼───────┼───────┼───────┼───────┤                      ├───────┼───────┼───────┼───────┼───────┼───────┤
       KC_LSFT,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                          KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN, KC_BSPC,
    //├────────┼───────┼───────┼───────┼───────┼───────┼───────┐      ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤ 
@@ -39,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //├───────┼───────┼───────┼───────┼───────┼───────┼───────┐      ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤ 
       ESCLCTL,KC_PERC,KC_LBRC,KC_RBRC,KC_MINS,KC_PIPE, TG_LOWR,      TG_RAIS,KC_CIRC, KC_DLR, KC_X,  KC_EQL,KC_ASTR,KC_SLSH, 
    //└───────┴───────┴───────┴───┬───┴───┬───┴───┬───┴───┬───┘      └───┬───┴───┬───┴───┬───┴───┬───┼───────┼───────┼───────┤
-                                  KC_LGUI,TT_LOWR, KC_SPC,               KC_SPC, TT_RAIS,KC_TILD
+                                  KC_LGUI,TT_LOWR, KC_SPC,               KC_SPC, TT_RAIS,KC_RALT
     //                           └───────┴───────┴───────┘              └───────┴───────┴───────┘ 
   ),
 
   [_RAISE] = LAYOUT(
    //┌────────┬───────┬───────┬───────┬───────┬───────┐                      ┌───────┬───────┬───────┬───────┬───────┬───────┐
-      KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6 ,                        KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+      KC_ESC, KC_1,    KC_2,  KC_3,    KC_4,  KC_5 ,                          KC_6,  KC_7,    KC_8,  KC_9,   KC_0,    KC_PIPE,
    //├────────┼───────┼───────┼───────┼───────┼───────┤                      ├───────┼───────┼───────┼───────┼───────┼───────┤
-       KC_GRV, KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_QUOT,                       KC_PIPE,KC_ASTR,KC_RBRC, KC_AT, KC_EXLM,KC_PIPE, 
+       KC_GRV, KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_QUOT,                       KC_PIPE,KC_ASTR,KC_RBRC, KC_AT, KC_PGUP,KC_PGDN, 
    //├────────┼───────┼───────┼───────┼───────┼───────┤                      ├───────┼───────┼───────┼───────┼───────┼───────┤
       KC_LSFT, KC_HASH,KC_DLR, KC_LPRN,KC_RPRN, KC_DQT,                       KC_AMPR,KC_LPRN,KC_RPRN,KC_COLN,KC_EQL, KC_BSPC, 
    //├────────┼───────┼───────┼───────┼───────┼───────┼───────┐      ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤ 
-      ESCLCTL, KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,TG_LOWR,      TG_RAIS,KC_UNDS, KC_DOT,  KC_LT,  KC_GT, KC_SLSH,KC_ESC, 
+      ESCLCTL, KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,TG_LOWR,      TG_RAIS,KC_UNDS,  KC_UP, KC_DOWN,KC_DOT, KC_SLSH,KC_ESC, 
    //└────────┴───────┴───────┴───┬───┴───┬───┴───┬───┴───┬───┘      └───┬───┴───┬───┴───┬───┴───┬───┼───────┼───────┼───────┤
                                    KC_LGUI,TT_LOWR, KC_SPC,               KC_ENT, TT_RAIS ,KC_RALT
     //                            └───────┴───────┴───────┘              └───────┴───────┴───────┘ 
